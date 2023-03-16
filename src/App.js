a903d46dab76bc45a6c886581c00e49851ab6d59
Oscar Puerto <oscar.puerto@sallinggroup.com>
2023-03-16 22:33:04 +0100

    Display the opening hours from each bilka store

--- src/App.js
+++ src/App.js
@@ -12,13 +12,28 @@ function App() {
     });
   }, []);
 
+  const renderOpeningHoursByStore = (store) => {
+    const openingHours = store.hours.filter((hour) => hour.type === 'store');
+    console.log(openingHours);
+
+      return (
+      <div>
+        <h3>{store.name} - Opening Hours</h3>
+        {openingHours.map((hours) => (
+           <p>open from {new Date(hours.open).getHours()} to {new Date(hours.close).getHours()}</p>  
+        ))}
+         
+      </div>
+      )
+  }
+
  
   return (
     <div className="App">
      <h1>Bilka stores - opening hours</h1>
 
       <div>
-        {data ? data.map((store) => (<div>{JSON.stringify(store, undefined, 3)}</div>)) : "Loading..."}
+        {data ? data.map((store) => (<div>{renderOpeningHoursByStore(store)}</div>)) : "Loading..."}
       </div>
   
     </div>

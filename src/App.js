6d223a0325be36879da5ab8f913771140e94a517
Oscar Puerto <oscar.puerto@sallinggroup.com>
2023-03-16 22:09:24 +0100

    Fetch the stores opening hours

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api").then( resp => resp.json()).then(data => {
+      console.log(data);
+     setData(data) 
+    });
+  }, []);
+
+ 
   return (
     <div className="App">
-     <h1>Salling group stores - opening hours</h1>
+     <h1>Bilka stores - opening hours</h1>
+
+      <div>
+        {data ? data.map((store) => (<div>{JSON.stringify(store, undefined, 3)}</div>)) : "Loading..."}
+      </div>
+  
     </div>
   );
 }

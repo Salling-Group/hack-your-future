17b6590b48bfa2c224fb020874c1c8c5503e13eb
Oscar Puerto <oscar.puerto@sallinggroup.com>
2023-03-17 10:47:21 +0100

    app and server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const apiToken = "7c6108c9-20ae-496e-93b0-64242ee04c69";
+
+// https://developer.sallinggroup.com/api-reference#apis-stores
+app.get('/api/', async (req, res) => {
+  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});

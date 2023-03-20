56681478d59e605b1891429ee57eda18b4ff0f04
Oscar Puerto <oscar.puerto@sallinggroup.com>
2023-03-20 09:15:22 +0100

    rename endpoint

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const port = 3001;
 const apiToken = "7c6108c9-20ae-496e-93b0-64242ee04c69";
 
 // https://developer.sallinggroup.com/api-reference#apis-stores
-app.get('/api/', async (req, res) => {
+app.get('/stores/', async (req, res) => {
   
 });
 

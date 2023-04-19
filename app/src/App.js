99652f3df82d6fd49ac5c72bd09fd3e8ce8cc8b2
Dmytrii Datskevych <datskevychdv@gmail.com>
2023-04-19 15:17:26 +0200

    Added tasks 1, 2, 3, 4 and bonus task

--- app/src/App.js
+++ app/src/App.js
@@ -4,48 +4,88 @@ import { useState } from 'react';
 function App() {
 
   const [data, setData] = useState(null);
-
+  const [error, setError] = useState(null);
 
   const fetchStoreOpeningHours = () => {
-    fetch("/stores/").then( resp => resp.json()).then(data => {
-      setData(data) 
-     });
+    fetch("/stores/")
+      .then(resp => resp.json())
+      .then(data => {
+        setData(data);
+        setError(null);
+      })
+      .catch(error => {
+        console.error("Error fetching store opening hours:", error);
+        setError("Failed to fetch store opening hours. Please try again later.");
+        setData(null);
+      });
   }
 
+
   const fetchStoreOpeningHoursBilkaTilts = () => {
-    fetch("/stores/efba0457-090e-4132-81ba-c72b4c8e7fee").then( resp => resp.json()).then(data => {
-      setData(data) 
-     });
+    fetch("/stores/efba0457-090e-4132-81ba-c72b4c8e7fee")
+      .then(resp => resp.json())
+      .then(data => {
+        setData(data);
+        setError(null);
+      })
+      .catch(error => {
+        console.error("Error fetching store opening hours:", error);
+        setError("Failed to fetch store opening hours. Please try again later.");
+        setData(null);
+      });
   }
 
   const fetchStoreOpeningHoursByName = () => {
-    fetch("/sorted-stores/").then( resp => resp.json()).then(data => {
-      setData(data) 
-     });
+    fetch("/sorted-stores/")
+      .then(resp => resp.json())
+      .then(data => {
+        setData(data);
+        setError(null);
+      })
+      .catch(error => {
+        console.error("Error fetching store opening hours:", error);
+        setError("Failed to fetch store opening hours. Please try again later.");
+        setData(null);
+      });
   }
 
   const showNearByStoreWithing10 = () => {
-    fetch("/find-nearby-stores/10").then( resp => resp.json()).then(data => {
-      if(!data.length) return;
-      setData(data) 
-     });
+    fetch("/find-nearby-stores/10")
+      .then(resp => resp.json())
+      .then(data => {
+        setData(data);
+        setError(null);
+      })
+      .catch(error => {
+        console.error("Error fetching store opening hours:", error);
+        setError("Failed to fetch store opening hours. Please try again later.");
+        setData(null);
+      });
   }
 
   const showNearByStoreWithing50 = () => {
-    fetch("/find-nearby-stores/50").then( resp => resp.json()).then(data => {
+    fetch("/find-nearby-stores/50")
+    .then( resp => resp.json())
+    .then(data => {
       if(!data.length) return;
-      setData(data) 
-     });
+      setData(data);
+      setError(null);
+    })
+    .catch(error => {
+      console.error("Error fetching store opening hours:", error);
+      setError("Failed to fetch store opening hours. Please try again later.");
+      setData(null);
+    });
   }
 
   const renderOpeningHoursByStore = (store) => {
     const openingHours = store.hours.filter((hour) => hour.type === 'store');
 
-    const weekdays = ["Sunday", "Monday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
       return (
-      <div>
-        <h3>{store.name} - Opening Hours</h3>
+      <div className='listItem'>
+        <h3>{store.name}<br></br>Opening Hours</h3>
         {openingHours.map((hours) => (
            <p>{weekdays[new Date(hours.date).getDay()]} open from {new Date(hours.open).getHours()} to {new Date(hours.close).getHours()}</p>  
         ))}
@@ -58,27 +98,28 @@ function App() {
   return (
     <div className="App">
 
-     <h1>Bilka stores - opening hours</h1>
-    <div className='button-wrapper'>
-    <button onClick={fetchStoreOpeningHours}>Fetch bilka opening hours</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={fetchStoreOpeningHoursBilkaTilts}>Fetch bilka Tilts</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={fetchStoreOpeningHoursByName}>Sorted by name asc</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={showNearByStoreWithing10}>Show stores within 10 Km</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={showNearByStoreWithing50}>Show stores within 50 Km</button>
-    </div>
+      <h1>Bilka stores - opening hours</h1>
+      <div className="wrapper">
+        <div className='button-wrapper'>
+        <button className='btn1' onClick={fetchStoreOpeningHours}>Fetch bilka opening hours</button>
+        </div>
+        <div className='button-wrapper'>
+        <button className='btn2' onClick={fetchStoreOpeningHoursBilkaTilts}>Fetch bilka Tilts</button>
+        </div>
+        <div className='button-wrapper'>
+        <button className='btn3' onClick={fetchStoreOpeningHoursByName}>Sorted by name asc</button>
+        </div>
+        <div className='button-wrapper'>
+        <button className='btn4' onClick={showNearByStoreWithing10}>Show stores within 10 Km</button>
+        </div>
+        <div className='button-wrapper'>
+        <button className='btn5' onClick={showNearByStoreWithing50}>Show stores within 50 Km</button>
+        </div>
+      </div>
       
-     
       
-
-      <div>
+      <div className="list">
+        {error && <div className="error">{error}</div>}
         {data && data.map((store) => (<div>{renderOpeningHoursByStore(store)}</div>))}
       </div>
   
--- server/index.js
+++ server/index.js
@@ -4,8 +4,122 @@ const port = 3001;
 const apiToken = "7c6108c9-20ae-496e-93b0-64242ee04c69";
 
 // https://developer.sallinggroup.com/api-reference#apis-stores
+//-------------------------------------------------------------------------# Task one:
 app.get('/stores/', async (req, res) => {
-  
+  try {
+    const response = await fetch("https://api.sallinggroup.com/v2/stores", {
+      headers: {
+        'Authorization': `Bearer ${apiToken}`
+      }
+    });
+
+    if (!response.ok){
+      res.status(404).json({message: 'Not Found'});
+    }
+    const data = await response.json();
+    const stores = data.map(element => {
+      return  {
+        name: element.name,
+        hours: element.hours
+      }
+   })
+    res.json(stores); 
+    
+  }
+  catch (error) {
+    res.status(500).json({message: 'Internal Server Error'});
+  }
+});
+
+
+//------------------------------------------------------------------------# Task two:
+app.get('/sorted-stores/', async (req, res) => {
+  try {
+    const response = await fetch("https://api.sallinggroup.com/v2/stores", {
+      headers: {
+        'Authorization': `Bearer ${apiToken}`
+      }
+    });
+
+    if (!response.ok){
+      res.status(404).json({message: 'Not Found'});
+    }
+    const data = await response.json();
+    const sortedStores = data.sort((a, b) => {
+      return a.name.localeCompare(b.name);
+    });
+    const stores = sortedStores.map(element => {
+      return  {
+        name: element.name,
+        hours: element.hours
+      }
+   })
+    res.json(stores); 
+  }
+  catch (error) {
+    res.status(500).json({message: 'Internal Server Error'});
+  }
+});
+
+
+//---------------------------------------------------------------------# Task three:
+app.get('/stores/:storeId', async (req, res) => {
+  try {
+    const storeId = req.params.storeId;
+
+    const response = await fetch(`https://api.sallinggroup.com/v2/stores/${storeId}`, {
+      headers: {
+        accept: 'application/json',
+        'Authorization': `Bearer ${apiToken}`
+      }
+    });
+
+    if (!response.ok){
+      res.status(404).json({message: 'Not Found'});
+    }
+    const data = await response.json();
+    const stores = [data].map(element => {
+      return  {
+        name: element.name,
+        hours: element.hours
+      }
+   })
+    res.json(stores);     
+  }
+  catch (error) {
+    res.status(500).json({message: 'Internal Server Error'});
+  }
+});
+
+
+//----------------------------------------------------------------------------- # Task four:
+app.get('/find-nearby-stores/:distance', async (req, res) => {
+  try {
+    const distance = req.params.distance;
+
+    const response = await fetch(`https://api.sallinggroup.com/v2/stores?geo=56.162387%2C10.0078135&page=1&per_page=100&radius=${distance}`, {
+      headers: {
+        accept: 'application/json',
+        'Authorization': `Bearer ${apiToken}`
+      }
+    });
+
+    if (!response.ok){
+      res.status(404).json({message: 'Not Found'});
+    }
+    const data = await response.json();
+    const stores = data.map(element => {
+      return  {
+        name: element.name,
+        hours: element.hours
+      }
+   })
+    res.json(stores); 
+ 
+  }
+  catch (error) {
+    res.status(500).json({message: 'Internal Server Error'});
+  }
 });
 
 app.listen(port, () => {

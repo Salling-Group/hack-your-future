a2429e6b4f8e34286895fbcf1bbad6c2576bf626
Mima <mimmamenser@yahoo.com>
2023-05-02 21:05:14 +0200

    Salling Group challenge files were added

--- app/src/App.js
+++ app/src/App.js
@@ -3,88 +3,88 @@ import { useState } from 'react';
 
 function App() {
 
-  const [data, setData] = useState(null);
+    const [data, setData] = useState(null);
 
 
-  const fetchStoreOpeningHours = () => {
-    fetch("/stores/").then( resp => resp.json()).then(data => {
-      setData(data) 
-     });
-  }
+    const fetchStoreOpeningHours = () => {
+        fetch("/stores/").then(resp => resp.json()).then(data => {
+            setData(data)
+        });
+    }
 
-  const fetchStoreOpeningHoursBilkaTilts = () => {
-    fetch("/stores/efba0457-090e-4132-81ba-c72b4c8e7fee").then( resp => resp.json()).then(data => {
-      setData(data) 
-     });
-  }
+    const fetchStoreOpeningHoursBilkaTilts = () => {
+        fetch("/stores/efba0457-090e-4132-81ba-c72b4c8e7fee").then(resp => resp.json()).then(data => {
+            setData(data)
+        });
+    }
 
-  const fetchStoreOpeningHoursByName = () => {
-    fetch("/sorted-stores/").then( resp => resp.json()).then(data => {
-      setData(data) 
-     });
-  }
+    const fetchStoreOpeningHoursByName = () => {
+        fetch("/sorted-stores/").then(resp => resp.json()).then(data => {
+            setData(data)
+        });
+    }
 
-  const showNearByStoreWithing10 = () => {
-    fetch("/find-nearby-stores/10").then( resp => resp.json()).then(data => {
-      if(!data.length) return;
-      setData(data) 
-     });
-  }
+    const showNearByStoreWithing10 = () => {
+        fetch("/find-nearby-stores/10").then(resp => resp.json()).then(data => {
+            if (!data.length) return;
+            setData(data)
+        });
+    }
 
-  const showNearByStoreWithing50 = () => {
-    fetch("/find-nearby-stores/50").then( resp => resp.json()).then(data => {
-      if(!data.length) return;
-      setData(data) 
-     });
-  }
+    const showNearByStoreWithing50 = () => {
+        fetch("/find-nearby-stores/50").then(resp => resp.json()).then(data => {
+            if (!data.length) return;
+            setData(data)
+        });
+    }
 
-  const renderOpeningHoursByStore = (store) => {
-    const openingHours = store.hours.filter((hour) => hour.type === 'store');
+    const renderOpeningHoursByStore = (store) => {
+        const openingHours = store.hours.filter((hour) => hour.type === 'store');
 
-    const weekdays = ["Sunday", "Monday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        const weekdays = ["Sunday", "Monday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-      return (
-      <div>
-        <h3>{store.name} - Opening Hours</h3>
-        {openingHours.map((hours) => (
-           <p>{weekdays[new Date(hours.date).getDay()]} open from {new Date(hours.open).getHours()} to {new Date(hours.close).getHours()}</p>  
-        ))}
-         
-      </div>
-      )
-  }
+        return (
+            <div>
+                <h3>{store.name} - Opening Hours</h3>
+                {openingHours.map((hours) => (
+                    <p>{weekdays[new Date(hours.date).getDay()]} open from {new Date(hours.open).getHours()} to {new Date(hours.close).getHours()}</p>
+                ))}
 
- 
-  return (
-    <div className="App">
+            </div>
+        )
+    }
 
-     <h1>Bilka stores - opening hours</h1>
-    <div className='button-wrapper'>
-    <button onClick={fetchStoreOpeningHours}>Fetch bilka opening hours</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={fetchStoreOpeningHoursBilkaTilts}>Fetch bilka Tilts</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={fetchStoreOpeningHoursByName}>Sorted by name asc</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={showNearByStoreWithing10}>Show stores within 10 Km</button>
-    </div>
-    <div className='button-wrapper'>
-    <button onClick={showNearByStoreWithing50}>Show stores within 50 Km</button>
-    </div>
-      
-     
-      
 
-      <div>
-        {data && data.map((store) => (<div>{renderOpeningHoursByStore(store)}</div>))}
-      </div>
-  
+    return (
+        <div className="App">
 
-    </div>
-  );
+            <h1>Bilka stores - opening hours</h1>
+            <div className='button-container'>
+                <div className='button-wrapper'>
+                    <button onClick={fetchStoreOpeningHours}>Fetch bilka opening hours</button>
+                </div>
+                <div className='button-wrapper'>
+                    <button onClick={fetchStoreOpeningHoursBilkaTilts}>Fetch bilka Tilts</button>
+                </div>
+                <div className='button-wrapper'>
+                    <button onClick={fetchStoreOpeningHoursByName}>Sorted by name asc</button>
+                </div>
+                <div className='button-wrapper'>
+                    <button onClick={showNearByStoreWithing10}>Show stores within 10 Km</button>
+                </div>
+                <div className='button-wrapper'>
+                    <button onClick={showNearByStoreWithing50}>Show stores within 50 Km</button>
+                </div>
+            </div>
+
+            <div>
+                {data && data.map((store) => (<div>{renderOpeningHoursByStore(store)}</div>))}
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
+
--- server/index.js
+++ server/index.js
@@ -1,13 +1,107 @@
 const express = require('express');
+require('dotenv').config();
 const app = express();
 const port = 3001;
-const apiToken = "7c6108c9-20ae-496e-93b0-64242ee04c69";
+const apiToken = process.env.API_TOKEN;
+const apiUrl = process.env.API_URL;
+
+const headers = {
+    'accept': 'application/json',
+    'Authorization': `Bearer ${apiToken}`
+}
+
+function filterAndMapData(data) {
+    let newArray = data
+        .filter(obj => obj.hours.some(g => g.type === 'store'))
+        .map(obj => ({ name: obj.name, hours: obj.hours }));
+
+    return newArray;
+}
+
 
 // https://developer.sallinggroup.com/api-reference#apis-stores
+// Task one: Create an endpoint /stores/
 app.get('/stores/', async (req, res) => {
-  
+    try {
+        const page = req.query.page ? parseInt(req.query.page) : 1;
+        const pageSize = req.query.pageSize ? parseInt(req.query.pageSize) : 100;
+        const stores = await fetch(`${apiUrl}?page=${page}&pageSize=${pageSize}`, { headers });
+
+        if (!stores.ok) {
+            res.status(404).json({ message: 'Data not available' });
+        }
+        const storesData = await stores.json();
+
+        let modifiedStoresData = filterAndMapData(storesData);
+        res.json(modifiedStoresData);
+
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+
+// Task two: Create an endpoint /sorted-stores/
+app.get('/sorted-stores/', async (req, res) => {
+    try {
+        const results = await fetch(`${apiUrl}`, { headers });
+
+        const data = await results.json();
+
+        const sortedStores = data.sort(function (a, b) {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+
+            return nameA.localeCompare(nameB);
+        });
+
+        let modifiedSortedStores = filterAndMapData(sortedStores);
+        res.send(modifiedSortedStores);
+
+    } catch (error) {
+        res.status(500).send({ message: error });
+    }
+})
+
+
+// Task three: Create an endpoint /stores/:storeId
+app.get('/stores/:storeId', async (req, res) => {
+    try {
+        const id = req.params.storeId;
+
+        const searchedStore = await fetch(`${apiUrl}/${id}`, { headers });
+
+        const storesData = await searchedStore.json();
+
+        let modifiedStoreData = filterAndMapData([storesData]);
+        res.json(modifiedStoreData);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message }); 
+    }
+});
+
+
+// Task four: Create an endpoint /find-nearby-stores/:distance
+app.get('/find-nearby-stores/:distance', async (req, res) => {
+    try {
+        const geo = encodeURIComponent('56.162387,10.0078135');
+        const distance = req.params.distance;
+
+        const stores = await fetch(`${apiUrl}?geo=${geo}&distance=${distance}`, { headers });
+
+        const storeList = await stores.json();
+
+        let modifiedStoreList = filterAndMapData(storeList);
+        res.json(modifiedStoreList);
+
+    } catch (error) {
+        res.status(500).send({ message: error });
+    }
+
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 });
+

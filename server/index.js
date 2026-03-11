c9c0bf1637dc49f594e461aedebfcb2fb32e35f5
Christian Hartøft-Nielsen <christian.hartoeft-nielsen@sallinggroup.com>
2026-03-11 11:08:46 +0100

    update apiToken and reference link

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-const apiToken = "7c6108c9-20ae-496e-93b0-64242ee04c69";
+const apiToken = 'SG_APIM_F7HPJKZCXZJGX5PAV1Z093S2SWCBMKSYY6AAKZTZ25WJPA3BKWNG';
 
-// https://developer.sallinggroup.com/api-reference#apis-stores
-app.get('/stores/', async (req, res) => {
-  
-});
+// https://developer.sallinggroup.dev/apireference/stores
+app.get('/stores/', async (req, res) => {});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 });
